4607e31b12d9ebc72ed2eca1d7bf9d481b0ce91d
ShahinT <shahin.tamjidi@gmail.com>
2024-12-27 21:08:34 +0100

    Initial commit

---
+++ Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    [MaxLength(256)]
+    public required string UserName { get; set; }
+}
